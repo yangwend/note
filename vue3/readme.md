7dcb207909dee73265c1e87229676a31ec7360f7
yangwend <1623177890@qq.com>
2020-07-23 13:51:54 +0800

    U

--- vue3/readme.md
+++ vue3/readme.md
@@ -1,3 +1,6 @@
 ## vue3 全家桶初体验
 
-https://www.yuque.com/gr481u/vmmtem/bd2lzo
+1. [语雀文档](https://www.yuque.com/gr481u/vmmtem/bd2lzo)
+
+2. [vue3 文档](https://v3.vuejs.org/)
+
---
+++ vue3/vue2-all/src/views/Test.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h2>about count: {{ count }}</h2>
+    <div v-html="html" class="myimg"></div>
+  </div>
+</template>
+<script>
+export default {
+  name: "test",
+  data() {
+    return {
+      count: 0,
+      html: `<p><span>w4rwrwrew</span><img src="https://static.yyfax.com/group1/M00/00/4F/wKgKGl7Q8bSAC1LtAAan1kt_mpw272.png" width="600" height="300"></p>`
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+.about {
+  color: #000;
+
+  .myimg {
+    img {
+      width: 100% !important;
+    }
+  }
+}
+</style>
